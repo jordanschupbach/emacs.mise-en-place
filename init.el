11449bc1de6c6daa96af32443df7ceeef7491259
jordan <jordans1882@gmail.com>
2026-03-24 05:27:31 -0600

    Attempt to use straight for ess

--- init.el
+++ init.el
@@ -18,6 +18,29 @@
 (package-initialize)
 
 
+
+;; Bootstrap straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
+
+
+
 (load-theme 'modus-vivendi)
 (load (concat config-dir "./packages/init.el"))
 (load (concat config-dir "./src/mep-config.el"))
@@ -27,6 +50,7 @@
 
 ;; Don't show warnings from libraries - only errors:
 (setq warning-minimum-level :error)
+
 
 
 
