9395af22c20756c4c86de9091ba6964910e8eaf5
jordan <jordans1882@gmail.com>
2026-03-25 08:35:35 -0600

    Revert to just passing args

--- flake.nix
+++ flake.nix
@@ -11,27 +11,19 @@
         system = system;
         overlays = [ (import overlay) ];
       };
-emacsWrapper = pkgs.writeShellScriptBin "emacs-wrapper" ''
-  #!/bin/sh
-  CACHE_DIR="$HOME/.emacs.mise-en-place"
-  mkdir -p "$CACHE_DIR"
-  cp -r ${toString ./.}/* "$CACHE_DIR/" 2>/dev/null || true
-  chmod -R u+w "$CACHE_DIR"/*
-  export PATH="${pkgs.cmake}/bin:$PATH"
-  export PATH="${pkgs.gnumake}/bin:$PATH"
-  export PATH="${pkgs.gcc}/bin:$PATH"
-  export PATH="${pkgs.libtool}/bin:$PATH"
-  export PATH="${pkgs.R}/bin:$PATH"
-
-  # Check if any args are provided to execute different behavior
-  if [ "$#" -eq 0 ]; then
-    exec ${pkgs.emacs-unstable}/bin/emacs --init-dir "$CACHE_DIR"
-  elif [ "$1" = "--version" ]; then
-    exec ${pkgs.emacs-unstable}/bin/emacs --version
-  else
-    exec ${pkgs.emacs-unstable}/bin/emacs --init-dir "$CACHE_DIR" "$@"
-  fi
-'';
+      emacsWrapper = pkgs.writeShellScriptBin "emacs-wrapper" ''
+        #!/bin/sh
+        CACHE_DIR="$HOME/.emacs.mise-en-place"
+        mkdir -p "$CACHE_DIR"
+        cp -r ${toString ./.}/* "$CACHE_DIR/" 2>/dev/null || true
+        chmod -R u+w "$CACHE_DIR"/*
+        export PATH="${pkgs.cmake}/bin:$PATH"
+        export PATH="${pkgs.gnumake}/bin:$PATH"
+        export PATH="${pkgs.gcc}/bin:$PATH"
+        export PATH="${pkgs.libtool}/bin:$PATH"
+        export PATH="${pkgs.R}/bin:$PATH"
+        exec ${pkgs.emacs-unstable}/bin/emacs --init-dir "$CACHE_DIR" "$@"
+      '';
     in
     {
       packages.${system}.default = pkgs.emacs-unstable;
