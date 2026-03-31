76a042fb8975ca83ea0788a502654f246e2613b7
jordanschupbach <jordans1882@gmail.com>
2026-03-30 20:30:11 -0600

    Small modifications to org-roam

--- packages/mep-org-roam.el
+++ packages/mep-org-roam.el
@@ -20,11 +20,11 @@
   ;;      :repo "org-roam/org-roam")
   :ensure t
   :custom
-  (org-roam-directory (file-truename "~/org-roam"))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol)
+    (org-roam-directory (file-truename "~/org-roam"))
+    ;; If you're using a vertical completion framework, you might want a more informative completion interface
+    ;; (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+    (org-roam-db-autosync-mode)
+    ;; If using org-roam-protocol
+    ;; (require 'org-roam-protocol)
   )
